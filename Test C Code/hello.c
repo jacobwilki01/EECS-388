c9a0b2ec1af8dd5ec0174ae7f244515fa7e9fa84
jacobwilki01 <jwilkus1@gmail.com>
2022-09-01 14:50:12 -0500

    Something

--- Test C Code/hello.c
+++ Test C Code/hello.c
@@ -1,10 +1,13 @@
-void add_val(int a) {
-    ++a;
-}
-
-void main()
-{
-    int var = 10;
-    add_val(var);
-    printf(var);
-}
+#include<stdio.h>  
+int main()  
+{   // initialize the local variables.  
+    int l, b, ar, pr;  
+    printf("Enter the length of the rectangle");  
+    scanf("%d", l); // take input from the user  
+    printf("Enter the breadth of the rectangle");  
+    scanf("%d", b);   
+    ar = l * b; // calculate the area of rectangle.  
+    pr = 2 * (l + b); // calculate the perimeter of rectangle.  
+    printf("\n Area of Rectangle is: %d", ar);  
+    printf("\n Perimeter of Rectangle is: %d", pr);    
+}  
